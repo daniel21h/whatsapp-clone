92ac6b8546166b4fc61fc6f9a7c3a7f080bf4fdf
daniel21h <hesseldaniel21@gmail.com>
2020-09-08 19:07:17 -0300

    feat: create aside header and search bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import DonutLargeIcon from '@material-ui/icons/DonutLarge'
+import ChatIcon from '@material-ui/icons/Chat'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import SearchIcon from '@material-ui/icons/Search'
+
+import './App.css'
 
 export default () => {
+  const [chatlist, setChatlist] = useState([
+    {},
+    {},
+    {},
+    {}
+  ])
+
   return (
-    <h1>Hello World</h1>
+    <div className="app-window">
+      <aside className="sidebar">
+        <header>
+          <img 
+            className="avatar"
+            src="https://api.adorable.io/avatars/285/abott@adorable.png" 
+            alt="Avatar"
+          />
+
+          <div className="header-buttons">
+            <div className="header-button">
+              <DonutLargeIcon style={{ color: '#919191' }} />
+            </div>
+
+            <div className="header-button">
+              <ChatIcon style={{ color: '#919191' }} />
+            </div>
+
+            <div className="header-button">
+              <MoreVertIcon style={{ color: '#919191' }} />
+            </div>
+          </div>
+        </header>
+
+        <div className="search">
+          <div className="search-input">
+            <SearchIcon fontSize="small" style={{ color: '#919191' }} />
+            <input 
+              type="search" 
+              placeholder="Procurar ou iniciar uma nova conversa" 
+            />
+          </div>
+        </div>
+
+        <main className="chatlist">
+          
+        </main>
+      </aside>
+      <section className="contentarea">
+        ...
+      </section>
+    </div>
   )
 }
