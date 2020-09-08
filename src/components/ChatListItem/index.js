7d553a939721d70c82c14a568a48f101a71738d0
daniel21h <hesseldaniel21@gmail.com>
2020-09-08 19:44:52 -0300

    feat: create ChatListItem component

---
+++ src/components/ChatListItem/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './styles.css'
+
+export default function ChatListItem() {
+  return (
+    <div className="chatitem">
+      <img 
+        className="chatitem-avatar"
+        src="https://api.adorable.io/avatars/285/abott@adorable.png
+        " 
+        alt="Avatar"
+      />
+
+      <section className="chatitem-lines">
+        <div className="chatitem-line">
+          <div className="chatitem-name">John Doe</div>
+          <div className="chatitem-date">19:00</div>
+        </div>
+        <div className="chatitem-line">
+          <div className="chatitem-lastmessage">
+            <p>Hello, what's up!</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
