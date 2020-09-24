b139c7fb5388d79e113aca9b067b1ec53627de43
daniel21h <hesseldaniel21@gmail.com>
2020-09-24 19:41:46 -0300

    feat: create user using Facebook

--- src/App.js
+++ src/App.js
@@ -1,51 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import ChatListItem from './components/ChatListItem'
-import ChatIntro from './components/ChatIntro';
-import ChatWindow from './components/ChatWindow';
+import ChatListItem from "./components/ChatListItem";
+import ChatIntro from "./components/ChatIntro";
+import ChatWindow from "./components/ChatWindow";
 
-import DonutLargeIcon from '@material-ui/icons/DonutLarge'
-import ChatIcon from '@material-ui/icons/Chat'
-import MoreVertIcon from '@material-ui/icons/MoreVert'
-import SearchIcon from '@material-ui/icons/Search'
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
 
-import './App.css'
-import NewChat from './components/NewChat';
-import Login from './components/Login';
+import "./App.css";
+import NewChat from "./components/NewChat";
+import Login from "./components/Login";
+import api from "./services/api";
 
 export default () => {
-  const [chatlist, setChatlist] = useState([
-    {chatId: 1, title: 'John Doe'},
-    {chatId: 2, title: 'Daniel Hessel'},
-    {chatId: 3, title: 'John Doe'},
-    {chatId: 4, title: 'John Doe'},
-    {chatId: 5, title: 'John Doe'},
-    {chatId: 6, title: 'John Doe'},
-    {chatId: 7, title: 'John Doe'},
-    {chatId: 8, title: 'John Doe'},
-  ])
+  const [chatlist, setChatlist] = useState([]);
 
-  const [activeChat, setActiveChat] = useState({})
-  const [user, setUser] = useState(null)
+  const [activeChat, setActiveChat] = useState({});
+  const [user, setUser] = useState(null);
 
-  const [showNewChat, setShowNewChat] = useState(false)
+  const [showNewChat, setShowNewChat] = useState(false);
 
   const handleNewChat = () => {
-    setShowNewChat(true)
-  }
+    setShowNewChat(true);
+  };
 
-  const handleLoginDatas = async (u) => {
+  const handleLoginDatas = async (user) => {
     let newUser = {
-      id: u.uid,
-      name: u.displayName,
-      avatar: u.photoURL
-    }
+      id: user.uid,
+      name: user.displayName,
+      avatar: user.photoURL,
+    };
 
-    setUser(newUser)
-  }
+    await api.addUser(newUser);
+
+    setUser(newUser);
+  };
 
   if (user === null) {
-    return <Login onReceive={handleLoginDatas} />
+    return <Login onReceive={handleLoginDatas} />;
   }
 
   return (
@@ -59,60 +53,50 @@ export default () => {
         />
 
         <header>
-          <img 
-            className="avatar"
-            src={user.avatar} 
-            alt="Avatar"
-          />
+          <img className="avatar" src={user.avatar} alt="Avatar" />
 
           <div className="header-buttons">
             <div className="header-button">
-              <DonutLargeIcon style={{ color: '#919191' }} />
+              <DonutLargeIcon style={{ color: "#919191" }} />
             </div>
 
             <div className="header-button" onClick={handleNewChat}>
-              <ChatIcon style={{ color: '#919191' }} />
+              <ChatIcon style={{ color: "#919191" }} />
             </div>
 
             <div className="header-button">
-              <MoreVertIcon style={{ color: '#919191' }} />
+              <MoreVertIcon style={{ color: "#919191" }} />
             </div>
           </div>
         </header>
 
         <div className="search">
           <div className="search-input">
-            <SearchIcon fontS ize="small" style={{ color: '#919191' }} />
-            <input 
-              type="search" 
-              placeholder="Procurar ou iniciar uma nova conversa" 
+            <SearchIcon fontS ize="small" style={{ color: "#919191" }} />
+            <input
+              type="search"
+              placeholder="Procurar ou iniciar uma nova conversa"
             />
           </div>
         </div>
 
         <main className="chatlist">
           {chatlist.map((item, key) => (
-            <ChatListItem 
-              key={key} 
+            <ChatListItem
+              key={key}
               data={item}
               active={activeChat.chatId === chatlist[key].chatId}
-              onClick={() => setActiveChat(chatlist[key])} 
+              onClick={() => setActiveChat(chatlist[key])}
             />
           ))}
         </main>
       </aside>
 
       <section className="contentarea">
-        {activeChat.chatId !== undefined && 
-          <ChatWindow
-            user={user}
-          />
-        }
-        
-        {activeChat.chatId === undefined &&
-          <ChatIntro />
-        }
+        {activeChat.chatId !== undefined && <ChatWindow user={user} />}
+
+        {activeChat.chatId === undefined && <ChatIntro />}
       </section>
     </div>
-  )
-}
+  );
+};
--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,29 @@
-import firebase, { firestore } from 'firebase/app'
-import 'firebase/firebase-auth'
-import 'firebase/firebase-firestore'
+import firebase from "firebase/app";
+import "firebase/firebase-auth";
+import "firebase/firebase-firestore";
 
-import firebaseConfig from './firebase.config'
+import firebaseConfig from "./firebase.config";
 
-const firebaseApp = firebase.initializeApp(firebaseConfig)
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-const db = firebaseApp.firestore()
-
+const db = firebaseApp.firestore();
 
 export default {
   fbLogin: async () => {
-    const provider = new firebase.auth.FacebookAuthProvider()
+    const provider = new firebase.auth.FacebookAuthProvider();
 
-    let result = await firebaseApp.auth().signInWithPopup(provider)
+    let result = await firebaseApp.auth().signInWithPopup(provider);
 
-    return result
-  }
-}
+    return result;
+  },
+
+  addUser: async (user) => {
+    await db.collection("users").doc(user.id).set(
+      {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      { merge: true }
+    );
+  },
+};
