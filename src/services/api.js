c7c256f5efaa62f2ae13abd24452e08938fcc932
daniel21h <hesseldaniel21@gmail.com>
2020-09-26 22:38:49 -0300

    .gitignore

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firebase-auth";
 import "firebase/firebase-firestore";
 
-import firebaseConfig from "./firebase.config";
+import firebaseConfig from "../../firebase/firebase.config";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
--- src/services/firebase.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyAMjbIey9Q_dc04lD2IgH4n3qqG5ugyOrU",
-  authDomain: "whatsapp-wcln.firebaseapp.com",
-  databaseURL: "https://whatsapp-wcln.firebaseio.com",
-  projectId: "whatsapp-wcln",
-  storageBucket: "whatsapp-wcln.appspot.com",
-  messagingSenderId: "382565784025",
-  appId: "1:382565784025:web:66f0713d449d420c5221c8"
-};
